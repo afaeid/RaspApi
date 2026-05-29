8dddd353f32326dce3c693e467d21b30ce670231
afaeid <muhammadafaeidsarkar@gmail.com>
2026-05-30 01:08:05 +0600

    Trying to fix deployment bygs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1);
+
 const morgan = require("morgan");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
